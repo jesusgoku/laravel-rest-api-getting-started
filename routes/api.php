fc825baa68fb3c21a88d43b713917b0a35a071f2
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-11-29 17:27:56 -0300

    chore(routes): remove dummy authenticated route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,4 @@
 <?php
-
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,9 +10,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
 
